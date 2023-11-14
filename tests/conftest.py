1d5da17b8ecd2cf5e03e3473121488c3f24cf6f2
waldemirrr <waldemirrrs@gmail.com>
2023-11-14 06:00:52 +0500

    added headless mode to selenium driver and github actions file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
     return driver
